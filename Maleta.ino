2dfad38303298257de6d07995315a2eb8d9d92d0
leofantucci <leo.fantucci@gmail.com>
2026-08-05 17:19:26 -0300

    Refatorando

---
+++ Maleta.ino
@@ -0,0 +1,801 @@
+// VERSAO 2 - Otimizando funcoes e corrigindo bugs
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Keypad.h>
+
+// ================= STRUCTS E ENUMS =================
+enum Tela {
+  INICIO,
+  MODO_JOGO,
+  MODO_ARMAR,
+  DURACAO,
+  MENU_SENHA,
+  MENU_CONFIRMAR_PONTOS,
+  MENU_PONTOS,
+  MENU_TEMPO_EXPLOSAO,
+  MENU_TEMPO_LIMITE,
+  VISUALIZAR_TEMPO_EXPLOSAO,
+  VISUALIZAR_TEMPO_LIMITE,
+  VISUALIZAR_SENHA,
+  VISUALIZAR_PONTOS,
+  DOMINANDO_1,
+  DOMINANDO_2,
+  DOMINADA,
+  CONFIRMAR,
+  JOGANDO,
+  ARMANDO,
+  ARMADA,
+  DESARMANDO,
+  FINAL,
+};
+
+enum Jogo {
+  NOVO,
+  ULTIMO,
+};
+
+enum ModoJogo {
+  SABOTAGEM,
+  DOMINACAO,
+};
+
+enum Duracao {
+  CQB, // 10 min
+  PADRAO, // 15 min
+  CAMPO_ABERTO,
+  PERSONALIZADA, // Tempo definido pelo usuario
+};
+
+enum ModoArmar {
+  BOTAO,
+  CARTAO,
+  SENHA,
+};
+
+struct Cartao {
+  byte uid[4];
+  String nome;
+};
+
+struct ScrollLCD {
+  int posicao;
+  unsigned long ultimoScroll;
+  const char* texto;
+};
+
+ScrollLCD menu = {0, 0, " (1) Sabotagem - (2) Dominacao"};
+ScrollLCD inicio = {0, 0, " (1) Novo Jogo - (2) Ultimo Jogo"};
+ScrollLCD duracaoSabotagem = {0, 0, " (1) CQB: 50s. - (2) Padrao: 1m30s. - (3) Campo Aberto: 4m20s. - (4) Personalizada"};
+ScrollLCD duracaoDominacao = {0, 0, " (1) CQB: 10min. - (2) Padrao: 30min. - (3) Campo Aberto: 60min. - (4) Personalizada"};
+ScrollLCD menuSabotagem = {0, 0, " (1) Botao - (2) Cartao - (3) Senha"};
+ScrollLCD menuDominacao = {0, 0, " (1) Botao - (2) Cartao"};
+ScrollLCD tempoSabotagem = {0, 0, " Defina o tempo de explosao apos armar (seg.)"};
+ScrollLCD tempoDominacao = {0, 0, " Defina o tempo limite da partida (seg.)"};
+ScrollLCD finalAtacantes = {0, 0, "    ATACANTES VENCERAM!"};
+ScrollLCD finalDefensores = {0, 0, "    DEFENSORES VENCERAM!"};
+ScrollLCD menuConfirmarPontos = {0, 0, " Deseja personalizar o max. de pontos? (PADRAO = (Tempo Lim./2)"};
+
+// ================= LCD I2C =================
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// ================= RFID ====================
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+// ================= BOTÕES ==================
+#define BTN_VERDE A1
+#define BTN_VERMELHO A2
+
+// ================ TECLADO ==================
+const byte LINHAS = 4;
+const byte COLUNAS = 4;
+
+char teclas[LINHAS][COLUNAS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+Cartao cartoes[] = {
+  {{0xAA, 0xBB, 0xCC, 0xDD}, "Vermelho"},
+  {{0x11, 0x22, 0x33, 0x44}, "Verde"},
+};
+
+const int NUM_CARTOES = sizeof(cartoes) / sizeof(cartoes[0]);
+
+byte pinosLinhas[LINHAS] = {2, 3, 4, 5};
+byte pinosColunas[COLUNAS] = {6, 7, 8, A0};
+
+Keypad teclado = Keypad(
+  makeKeymap(teclas),
+  pinosLinhas,
+  pinosColunas,
+  LINHAS,
+  COLUNAS
+);
+
+byte bloco[8] = {
+  B11111, B11111, B11111, B11111,
+  B11111, B11111, B11111, B11111
+};
+
+byte setaEsquerda[8] = {
+  B11100, B11110, B11111, B11111,
+  B11111, B11110, B11100, B00000
+};
+
+Tela telaAtual = INICIO;
+ModoJogo modoJogoAtual = SABOTAGEM;
+ModoArmar modoArmarAtual = BOTAO;
+Duracao duracaoAtual = PADRAO;
+
+Tela ultimaTela = INICIO;
+
+bool atualizouTela = false;
+bool atualizouModoJogo = false;
+bool atualizouModoArmar = false;
+bool atualizouDuracao = false;
+
+String textoScroll = "";
+int posicaoScroll = 0;
+unsigned long ultimoScroll = 0;
+unsigned long inicioExplosao = 0;
+unsigned long tempoMensagemVisualizar = 0;
+
+int etapaDecodificacao = 0;
+unsigned long ultimoAvancoDecodificacao = 0;
+int progressoAnimacao = 0;
+unsigned long ultimoFrameAnimacao = 0;
+
+String tempoExplosao = "50";
+String tempoExplosao_inserido = "";
+
+String tempoLimite = "600";
+String tempoLimite_inserido = "";
+
+String senha = "";
+String senha_inserida = "";
+
+// Variáveis de Controle do Modo Dominação
+int EquipeAtiva = 0;
+int jaFoi = 0;
+int pontosEq1 = 0;
+int pontosEq2 = 0;
+unsigned long tempoInicioDominacao = 0;
+unsigned long ultimoPontoContado = 0;
+int pos1 = 0;
+int pos2 = 0;
+int freq = 1000; // Tempo em ms para somar 1 ponto (1000ms = 1s)
+int maxPts = 300; // Pontuação máxima para vitória
+String maxPts_inserido = "";
+
+/* ========================= */
+/* FUNÇÕES                   */
+/* ========================= */
+String nomeModoJogoAtual(){
+  switch(modoJogoAtual){
+    case SABOTAGEM:
+      return "Sabotagem";
+    case DOMINACAO:
+      return "Dominacao";
+  }
+}
+String nomeModoArmarAtual(){
+  switch(modoArmarAtual){
+    case BOTAO:
+      return "Botao";
+    case CARTAO:
+      return "Cartao";
+    case SENHA:
+      return "Senha";
+  }
+}
+String nomeDuracaoAtual(){
+  switch(duracaoAtual){
+    case CQB:
+      return "CQB";
+    case PADRAO:
+      return "PADRAO";
+    case CAMPO_ABERTO:
+      return "CAMPO ABERTO";
+    case PERSONALIZADA:
+      return "PERSONALIZADA";
+  }
+}
+void (*reiniciarSoftware)(void) = 0;
+
+void scrollTexto(ScrollLCD &scroll, int linha, unsigned long intervalo) {
+    int tamOriginal = strlen(scroll.texto);
+    int tamTotal = tamOriginal + 16; 
+
+    if (millis() - scroll.ultimoScroll >= intervalo) {
+        scroll.ultimoScroll = millis();
+        lcd.setCursor(0, linha);
+
+        for (int i = 0; i < 16; i++) {
+            int indice = (scroll.posicao + i) % tamTotal;
+            if (indice < tamOriginal) {
+                lcd.print(scroll.texto[indice]);
+            } else {
+                lcd.print(' ');
+            }
+        }
+        scroll.posicao = (scroll.posicao + 1) % tamTotal;
+    }
+}
+
+void mudarTela(Tela novaTela) {
+  telaAtual = novaTela;
+  atualizouTela = false;
+  
+  // Reseta os índices de rolagem dos menus
+  menu.posicao = 0;
+  inicio.posicao = 0;
+  duracaoSabotagem.posicao = 0;
+  duracaoDominacao.posicao = 0;
+  menuSabotagem.posicao = 0;
+  menuDominacao.posicao = 0;
+  tempoSabotagem.posicao = 0;
+  tempoDominacao.posicao = 0;
+  finalAtacantes.posicao = 0;
+  finalDefensores.posicao = 0;
+
+  // Reseta estado do jogo quando retorna ao inicio
+  if (novaTela == INICIO) {
+    pontosEq1 = 0;
+    pontosEq2 = 0;
+    jaFoi = 0;
+    EquipeAtiva = 0;
+  }
+}
+
+void telaErroVazio(Tela antiga){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Digite um valor!");
+  delay(3000);
+  mudarTela(antiga);
+}
+
+void telaErro(Tela antiga){
+  lcd.clear();
+  lcd.setCursor(6,0);
+  lcd.print("ERRO");
+  delay(1000);
+  mudarTela(antiga);
+}
+
+void salvarSenha(){
+    if(senha_inserida == ""){
+        telaErroVazio(MENU_SENHA);
+    } else{
+      senha = senha_inserida;
+      Serial.println("SENHA SALVA: " + senha);
+      mudarTela(VISUALIZAR_SENHA);
+    }
+}
+
+void salvarPontos(){
+    if(maxPts_inserido == ""){
+        telaErroVazio(MENU_PONTOS);
+    } else{
+      maxPts = maxPts_inserido.toInt();
+      Serial.println("LIM. PONTOS SALVO: " + maxPts);
+      mudarTela(VISUALIZAR_PONTOS);
+    }
+}
+
+void salvarTempoExplosao(){
+    if(tempoExplosao_inserido == ""){
+      telaErroVazio(MENU_TEMPO_EXPLOSAO);
+    } else {
+      tempoExplosao = tempoExplosao_inserido;
+      mudarTela(VISUALIZAR_TEMPO_EXPLOSAO);
+    }
+}
+
+void salvarTempoLimite(){
+    if(tempoLimite_inserido == ""){
+      telaErroVazio(MENU_TEMPO_LIMITE);
+    } else {
+      tempoLimite = tempoLimite_inserido;
+      mudarTela(VISUALIZAR_TEMPO_LIMITE);
+    }
+}
+
+void mudarModoJogo(ModoJogo novoModo) {
+  modoJogoAtual = novoModo;
+  atualizouModoJogo = false;
+}
+
+void mudarModoArmar(ModoArmar novoModo) {
+  modoArmarAtual = novoModo;
+  atualizouModoArmar = false;
+}
+
+void mudarDuracao(Duracao novaDuracao) {
+  duracaoAtual = novaDuracao;
+  atualizouDuracao = false;
+}
+
+void barraCarregamento(int tempo) {
+  lcd.createChar(0, bloco);
+  unsigned long inicioTimer = millis();
+  int posicao = 0;
+
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+
+  while (millis() - inicioTimer < tempo) {
+    lcd.setCursor(posicao, 1);
+    lcd.write(byte(0));
+    delay(250);
+    posicao++;
+
+    if (posicao >= 16) {
+      posicao = 0;
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+    }
+  }
+}
+
+/* ========================= */
+/* TELAS                     */
+/* ========================= */
+
+void telaInicio() {
+  ultimaTela = INICIO;
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Bem-Vindo!");
+    atualizouTela = true;
+  }
+
+  scrollTexto(inicio, 1, 300);
+  
+  char tecla = teclado.getKey();
+  if (tecla == '1') {
+    mudarTela(MODO_JOGO);
+  }
+}
+
+void telaModoJogo() {
+  ultimaTela = INICIO;
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Escolha o modo:");
+    atualizouTela = true;
+  }
+
+  scrollTexto(menu, 1, 300);
+
+  char tecla = teclado.getKey();
+  if(tecla == 'B'){
+    mudarTela(ultimaTela);
+  }
+  if (tecla == '1') {
+    mudarModoJogo(SABOTAGEM);
+    mudarTela(MODO_ARMAR);
+  }
+  if (tecla == '2') {
+    mudarModoJogo(DOMINACAO);
+    mudarTela(MODO_ARMAR);
+  }
+}
+
+void telaModoArmar() {
+  ultimaTela = MODO_JOGO;
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Modo de armar:");
+    atualizouTela = true;
+  }
+  
+  char tecla = teclado.getKey();
+  if(tecla == 'B'){
+    mudarTela(ultimaTela);
+  }
+
+  switch(modoJogoAtual){
+    case SABOTAGEM:
+      scrollTexto(menuSabotagem, 1, 300);
+
+      if (tecla == '1') {
+        mudarModoArmar(BOTAO);
+        mudarTela(DURACAO);
+      }
+      if (tecla == '2') {
+        mudarModoArmar(CARTAO);
+        mudarTela(DURACAO);
+      }
+      if (tecla == '3') {
+        mudarModoArmar(SENHA);      
+        mudarTela(MENU_SENHA);
+      }
+      break;
+      
+    case DOMINACAO:
+      scrollTexto(menuDominacao, 1, 300);
+    
+      if (tecla == '1') {
+        mudarModoArmar(BOTAO);
+        mudarTela(DURACAO);
+      }
+      if (tecla == '2') {
+        mudarModoArmar(CARTAO);
+        mudarTela(DURACAO);
+      }
+      break;
+  }
+}
+
+void telaDuracao(){
+  ultimaTela = MODO_ARMAR;
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    if(modoJogoAtual == SABOTAGEM){
+      lcd.print("Tempo apos armar");
+    } else if(modoJogoAtual == DOMINACAO){
+      lcd.print("Limite de tempo");
+    }
+    atualizouTela = true;
+  }  
+
+  if(modoJogoAtual == SABOTAGEM){
+    scrollTexto(duracaoSabotagem, 1, 300);
+  }
+  if(modoJogoAtual == DOMINACAO){
+    scrollTexto(duracaoDominacao, 1, 300);
+  }
+  
+  char tecla = teclado.getKey();
+
+  if((tecla >= '1' && tecla <= '4') || tecla == 'B'){
+    switch(tecla){
+      case 'B':
+        mudarTela(ultimaTela);
+        break;
+      case '1':
+        mudarDuracao(CQB);
+        if(modoJogoAtual == SABOTAGEM) tempoExplosao = "50";
+        if(modoJogoAtual == DOMINACAO) { tempoLimite = "600"; maxPts = 300; }
+        mudarTela(VISUALIZAR_PONTOS);
+        break;
+      case '2':
+        mudarDuracao(PADRAO);
+        if(modoJogoAtual == SABOTAGEM) tempoExplosao = "90";
+        if(modoJogoAtual == DOMINACAO) { tempoLimite = "1800"; maxPts = 900; }
+        mudarTela(VISUALIZAR_PONTOS);
+        break;
+      case '3':
+        mudarDuracao(CAMPO_ABERTO);
+        if(modoJogoAtual == SABOTAGEM) tempoExplosao = "260";
+        if(modoJogoAtual == DOMINACAO) { tempoLimite = "3600"; maxPts = 1800; }
+        mudarTela(VISUALIZAR_PONTOS);
+        break;
+      case '4':
+        mudarDuracao(PERSONALIZADA);
+        if(modoJogoAtual == SABOTAGEM){
+          mudarTela(MENU_TEMPO_EXPLOSAO);
+        }
+        if(modoJogoAtual == DOMINACAO){
+          mudarTela(MENU_TEMPO_LIMITE);
+        }
+        break;
+    }
+  }
+}
+
+void telaMenuSenha() {
+  ultimaTela = MODO_ARMAR;
+  if (!atualizouTela) {
+    senha_inserida = "";
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Defina a senha:");
+  
+    atualizouTela = true;
+  }
+
+  char tecla = teclado.getKey();
+  if(tecla >= '0' && tecla <= '9'){
+    senha_inserida += tecla;
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print(senha_inserida);
+  }
+  if(tecla == 'D'){
+    senha_inserida = "";
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+  }
+  if(tecla == '#'){
+    salvarSenha();
+  }
+  if(tecla == 'B'){
+    mudarTela(ultimaTela);
+  }
+}
+
+void telaVisualizarSenha(){
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Senha salva");
+    lcd.setCursor(0,1);
+    lcd.print(senha);
+    atualizouTela = true;
+  }
+  delay(1000);
+  mudarTela(DURACAO);
+}
+
+void telaMenuConfirmarPontos(){
+  ultimaTela = MENU_TEMPO_LIMITE;
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0,1);
+    lcd.print("(1)Sim - (2)Nao");
+    atualizouTela = true;
+  }
+    
+  lcd.setCursor(0, 0);
+  scrollTexto(menuConfirmarPontos, 0, 300);
+  
+  char tecla = teclado.getKey();
+  
+  if(tecla == '1'){
+    mudarTela(MENU_PONTOS);
+  }
+  if(tecla == '2'){
+    maxPts = (tempoLimite.toInt())/2;
+    mudarTela(VISUALIZAR_PONTOS);
+  }
+  if(tecla == 'B'){
+    mudarTela(ultimaTela);
+  }
+}
+
+void telaMenuPontos() {
+  ultimaTela = MENU_CONFIRMAR_PONTOS;
+  if (!atualizouTela) {
+    maxPts_inserido = "";
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Def. max. pts.");
+    atualizouTela = true;
+  }
+  
+  char tecla = teclado.getKey();
+
+  if(tecla >= '0' && tecla <= '9'){
+    maxPts_inserido += tecla;
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print(maxPts_inserido);
+  }
+  if(tecla == 'D'){
+    maxPts_inserido = "";
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+  }
+  if(tecla == '#'){
+    salvarPontos();
+  }
+  if(tecla == 'B'){
+    mudarTela(ultimaTela);
+  }
+}
+
+void telaVisualizarPontos(){
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Max. de pontos");
+    lcd.setCursor(0,1);
+    lcd.print(maxPts);
+    atualizouTela = true;
+  }
+  delay(1000);
+  mudarTela(CONFIRMAR);
+}
+
+void telaTempoExplosao() {
+  ultimaTela = DURACAO;
+  if (!atualizouTela) {
+    tempoExplosao_inserido = "";
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Tempo apos armar");
+    atualizouTela = true;
+  }
+
+  char tecla = teclado.getKey();
+  if(tecla >= '0' && tecla <= '9'){
+    tempoExplosao_inserido += tecla;
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print(tempoExplosao_inserido);
+  }
+  if(tecla == 'D'){
+    tempoExplosao_inserido = "";
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+  }
+  if(tecla == '#'){
+    salvarTempoExplosao();
+  }
+  if(tecla == 'B'){
+    mudarTela(ultimaTela);
+  }
+}
+
+void telaVisualizarTempoExplosao(){
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Tempo explosao");
+    lcd.setCursor(0,1);
+    lcd.print(tempoExplosao + "seg.");
+    atualizouTela = true;
+  }
+  delay(1000);
+  mudarTela(CONFIRMAR);
+}
+
+void telaTempoLimite() {
+  ultimaTela = DURACAO;
+  if (!atualizouTela) {
+    tempoLimite_inserido = "";
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Tempo limite");
+    atualizouTela = true;
+  }
+
+  char tecla = teclado.getKey();
+  if(tecla >= '0' && tecla <= '9'){
+    tempoLimite_inserido += tecla;
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    lcd.print(tempoLimite_inserido);
+  }
+  if(tecla == 'D'){
+    tempoLimite_inserido = "";
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+  }
+  if(tecla == '#'){
+    salvarTempoLimite();
+  }
+  if(tecla == 'B'){
+    mudarTela(ultimaTela);
+  }
+}
+
+void telaVisualizarTempoLimite(){
+  if (!atualizouTela) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Tempo limite");
+    lcd.setCursor(0,1);
+    lcd.print(tempoLimite + "seg.");
+    atualizouTela = true;
+  }
+  delay(1000);
+  mudarTela(MENU_CONFIRMAR_PONTOS);
+}
+
+void telaConfirmar(){
+  if(!atualizouTela){
+    Serial.println(nomeModoJogoAtual());
+    Serial.println(nomeModoArmarAtual());
+    Serial.println(nomeDuracaoAtual());
+    if(modoJogoAtual == SABOTAGEM){
+      Serial.println("Tempo Exp.: " + tempoExplosao);
+      Serial.println("Senha: " + senha);
+    } else {
+      Serial.println("Tempo Lim.: " + tempoLimite);
+      Serial.println("Max Pts.: " + String(maxPts));
+    }
+    atualizouTela = true;
+  }
+
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  // RFID
+  SPI.begin();
+  rfid.PCD_Init();
+
+  // Botões
+  pinMode(BTN_VERDE, INPUT_PULLUP);
+  pinMode(BTN_VERMELHO, INPUT_PULLUP);
+
+  Serial.println("Sistema iniciado");
+};
+
+void loop() {
+  switch (telaAtual) {
+    case INICIO:
+      telaInicio();
+      break;
+     
+    case MODO_JOGO:
+      telaModoJogo();
+      break;
+
+    case MODO_ARMAR:
+      telaModoArmar();
+      break;
+
+    case MENU_SENHA:
+      telaMenuSenha();
+      break;
+
+    case VISUALIZAR_SENHA:
+      telaVisualizarSenha();
+      break;
+      
+    case DURACAO:
+      telaDuracao();
+      break;
+
+    case MENU_TEMPO_EXPLOSAO:
+      telaTempoExplosao();
+      break;
+    
+    case VISUALIZAR_TEMPO_EXPLOSAO:
+      telaVisualizarTempoExplosao();
+      break;
+
+    case VISUALIZAR_TEMPO_LIMITE:
+      telaVisualizarTempoLimite();
+      break;
+    
+    case MENU_TEMPO_LIMITE:
+      telaTempoLimite();
+      break;
+    
+    case MENU_CONFIRMAR_PONTOS:
+      telaMenuConfirmarPontos();
+      break;
+    
+    case MENU_PONTOS:
+      telaMenuPontos();
+      break;
+    
+    case VISUALIZAR_PONTOS:
+      telaVisualizarPontos();
+      break;
+      
+    case CONFIRMAR:
+      telaConfirmar();
+      break;
+
+    default:
+      mudarTela(INICIO);
+      break;
+  }
+}
